aac6fb7a0189d3a4d7cf40df393a7426fc16dd49
Vusal Aliyev <vusalaliev02@gmail.com>
2024-03-06 17:59:19 +0400

    Mail bug

--- LiquorStoreFinalProject/Controllers/ContactController.cs
+++ LiquorStoreFinalProject/Controllers/ContactController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
 
 namespace LiquorStoreFinalProject.Controllers
 {
@@ -7,6 +9,28 @@ namespace LiquorStoreFinalProject.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+        public async Task<IActionResult> SendEmail(string name, string email, string subject, string message)
+        {
+            var smtpClient = new SmtpClient("smtp.mail.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("vusalaliev00@mail.ru", "Jeyhun12*"),
+                EnableSsl = true,
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("vusalaliev00@mail.ru"),
+                Subject = subject,
+                Body = $"Name: {name}\nEmail: {email}\nMessage: {message}",
+                IsBodyHtml = false,
+            };
+
+            mailMessage.To.Add("vusalaliev02@gmail.com");
+
+            await smtpClient.SendMailAsync(mailMessage);
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
