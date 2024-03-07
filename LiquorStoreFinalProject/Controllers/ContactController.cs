022470a0644e0d0f949a868a084807061bcd4e75
Vusal Aliyev <vusalaliev02@gmail.com>
2024-03-08 00:50:56 +0400

    Yoxlama

--- LiquorStoreFinalProject/Controllers/ContactController.cs
+++ LiquorStoreFinalProject/Controllers/ContactController.cs
@@ -12,15 +12,15 @@ namespace LiquorStoreFinalProject.Controllers
         }
         public async Task<IActionResult> SendEmail(string name, string email, string subject, string message)
         {
-            var smtpClient = new SmtpClient("smtp.mail.ru")
+            var smtpClient = new SmtpClient("smtp.gmail.com")
             {
                 Port = 587,
-                Credentials = new NetworkCredential("vusalaliev00@mail.ru", "Jeyhun12*"),
+                Credentials = new NetworkCredential("vusalia@code.edu.az", "Vusalcode278!"),
                 EnableSsl = true,
             };
             var mailMessage = new MailMessage
             {
-                From = new MailAddress("vusalaliev00@mail.ru"),
+                From = new MailAddress("vusalia@code.edu.az"),
                 Subject = subject,
                 Body = $"Name: {name}\nEmail: {email}\nMessage: {message}",
                 IsBodyHtml = false,
